e36817ffc90b574eb555922356c04497679fdb8c
Nikola Svetoslavov <56298370+tsvetelinda@users.noreply.github.com>
2025-02-14 17:04:33 +0200

    Add the user type and user service

---
+++ frontend/src/app/types/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    "_id": string;
+    "firstName": string;
+    "lastName": string;
+    "email": string;
+    "birthDate": Date;
+    "phone": string;
+    "sport": string;
+    "skillLevel": string;
+}
---
+++ frontend/src/app/user/user.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
+import { User } from '../types/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private user$$ = new BehaviorSubject<User | null>(null);
+  private user$ = this.user$$.asObservable();
+
+  USER_KEY = '[user]';
+  user: User | null = null;
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor(private http: HttpClient) { 
+    this.user$.subscribe((user) => this.user = user);
+  }
+
+  register(firstName: string, lastName: string, email: string, password: string, birthDate: Date, phone: string, sport: string, skillLevel: string) {
+    return this.http.post<User>('/api/users/register', { firstName, lastName, email, password, birthDate, phone, sport, skillLevel })
+    .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<User>('/api/users/login', { email, password })
+    .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  getProfile() {
+    return this.http.get<User>('/api/users/profile')
+    .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  logout() {
+    return this.http.post('/api/users/logout', {})
+    .pipe(tap(() => this.user$$.next(null)));
+  }
+
+  editProfile(userId: string | undefined, firstName: string, lastName: string, email: string, birthDate: Date, phone: string, sport: string, skillLevel: string) {
+    return this.http.put<User>(`/api/users/${userId}`, { firstName, lastName, email, birthDate, phone, sport, skillLevel })
+    .pipe(tap((user) => this.user$$.next(user)));
+  }
+}
