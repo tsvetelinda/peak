51a95823d54678526e1ebe0b1da69232af79e65c
Nikola Svetoslavov <56298370+tsvetelinda@users.noreply.github.com>
2025-02-14 21:53:08 +0200

    Define the routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    {
+        path: 'login', 
+        loadComponent: () => import('./user/login/login.component').then((c) => c.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./user/register/register.component').then((c) => c.RegisterComponent)
+    },
+    /*{
+        path: '404', 
+        loadComponent: () => import('./error/error.component').then((c) => c.ErrorComponent)
+    },*/
+    {path: '**', redirectTo: '/404', pathMatch: 'full'},
+];
