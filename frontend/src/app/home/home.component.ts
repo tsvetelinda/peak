e9da9f4e93c32ea526d9b2d27c9f50a993719b2b
Nikola Svetoslavov <56298370+tsvetelinda@users.noreply.github.com>
2025-02-14 21:52:57 +0200

    Add the login and register components

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../core/header/header.component';
 import { MountainsComponent } from '../mountains/mountains/mountains.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, MountainsComponent],
+  imports: [HeaderComponent, MountainsComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css' 
 })
