791f0b77aa35d4c25b67bedc7c596b2fffa3467d
Nikola Svetoslavov <56298370+tsvetelinda@users.noreply.github.com>
2025-02-17 15:56:34 +0200

    Add login logic

--- frontend/src/app/user/login/login.component.ts
+++ frontend/src/app/user/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,4 +11,23 @@ import { UserService } from '../user.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  login() {
+    const { email, password } = this.form.value;
+
+    this.userService.login(email!, password!).subscribe({
+      next: () => {
+        this.router.navigate(['home']);
+      },
+      error: () => {
+        this.form.reset();
+      }
+    });
+  }
 }
