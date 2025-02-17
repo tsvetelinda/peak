40a89d613b401380fbe28d93bde7303e245affe4
Nikola Svetoslavov <56298370+tsvetelinda@users.noreply.github.com>
2025-02-17 16:55:34 +0200

    Update the route links of the navigation

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../core/header/header.component';
 import { MountainsComponent } from '../mountains/mountains/mountains.component';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +10,25 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css' 
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const fragment = this.route.snapshot.fragment;
+        if (fragment) {
+          this.scrollToElement(fragment);
+        }
+      }
+    });
+  }
+
+  private scrollToElement(fragment: string): void {
+    const element = document.getElementById(fragment);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
