0115c36f318846c2948bf12100169414e7b6b99a
Nikola Svetoslavov <56298370+tsvetelinda@users.noreply.github.com>
2025-02-18 22:47:12 +0200

    Update the design

--- frontend/src/app/user/login/login.component.ts
+++ frontend/src/app/user/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
