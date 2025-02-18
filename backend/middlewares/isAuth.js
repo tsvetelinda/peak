82caf23e76346462007406f56cfc326cd30a2f48
Nikola Svetoslavov <56298370+tsvetelinda@users.noreply.github.com>
2025-02-18 21:29:14 +0200

    Update isAuth.js

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -2,8 +2,6 @@ const jwt = require('jsonwebtoken');
 const { AUTH_COOKIE_NAME } = require('../constants.js');
 
 module.exports = (req, res, next) => {
-  console.log(req.headers);
-
   const token = req.cookies[AUTH_COOKIE_NAME];
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
