4f00e87a6cdcf3da20d3398fd961ef1339dde889
Nikola Svetoslavov <56298370+tsvetelinda@users.noreply.github.com>
2025-02-18 14:01:55 +0200

    Create a view-switcher component

---
+++ frontend/src/app/view-switcher/view-switcher.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-switcher',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-switcher.component.html',
+  styleUrl: './view-switcher.component.css'
+})
+export class ViewSwitcherComponent {
+
+}
