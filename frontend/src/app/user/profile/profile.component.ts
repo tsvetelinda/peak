e1ffb31b039b5f4333ff6d8f7d2543429d138a9f
Nikola Svetoslavov <56298370+tsvetelinda@users.noreply.github.com>
2025-03-10 11:16:20 +0200

    Update the user profile design

--- frontend/src/app/user/profile/profile.component.ts
+++ frontend/src/app/user/profile/profile.component.ts
@@ -12,6 +12,9 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   user: User | null = null;
+  skillIcon: string = '';
+  ageGroup: string = '';
+  avatarPath: string = '';
 
   constructor(private userService: UserService, private router: Router) { }
 
@@ -19,6 +22,9 @@ export class ProfileComponent implements OnInit {
     this.userService.getProfile().subscribe({
       next: (profile) => {
         this.user = profile;
+        this.setSkillIcon(this.user.skillLevel);
+        this.setAgeGroup(this.user.birthDate);
+        this.setAvatarPath(this.user.sport);
       },
       error: (err) => {
         console.log(err.error.message);
@@ -36,4 +42,66 @@ export class ProfileComponent implements OnInit {
       }
     });
   }
+
+  changePassword() {
+
+  }
+
+  editProfile() {
+
+  }
+
+  showPasses() {
+    
+  }
+
+  private setSkillIcon(skillLevel: string): void {
+    switch (skillLevel) {
+      case 'Beginner':
+        this.skillIcon = '/icons/gloves.svg';
+        break;
+      case 'Intermediate':
+        this.skillIcon = '/icons/goggles.svg';
+        break;
+      case 'Advanced':
+        this.skillIcon = '/icons/boots.svg';
+        break;
+    }
+  }
+
+  private setAgeGroup(birthdate: Date): void {
+    const age = this.calculateAge(birthdate);
+    if (age <= 18) {
+      this.ageGroup = 'Young';
+    } else if (age >= 60) {
+      this.ageGroup = 'Elder';
+    } else {
+      this.ageGroup = 'Adult';
+    }
+  }
+
+  private setAvatarPath(sport: string): void {
+    if (sport == 'Snowboarder') {
+      this.avatarPath = 'bear-avatar.png';
+    } else if (sport == 'Skier') {
+      this.avatarPath = 'pigeon-avatar.png';
+    }
+  }
+
+  private calculateAge(birthdate: Date): number {
+    const birthDate = new Date(birthdate);
+    const today = new Date();
+  
+    let age = today.getFullYear() - birthDate.getFullYear();
+    
+    const hasBirthdayPassed =
+      today.getMonth() > birthDate.getMonth() ||
+      (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
+  
+    if (!hasBirthdayPassed) {
+      age--;
+    }
+  
+    return age;
+  } 
 }
